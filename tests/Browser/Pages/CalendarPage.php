47770b0c9f7b842e0e1e7590ef869cb2fd7024e3
gabbydgab <codingmattersio@gmail.com>
2020-05-02 10:36:02 +0800

    changed the method for scheduling once a month event

--- tests/Browser/Pages/CalendarPage.php
+++ tests/Browser/Pages/CalendarPage.php
@@ -81,13 +81,18 @@ class CalendarPage extends Page
             ->check("#sun");
     }
 
-    public function scheduleOnceAMonthFor(Browser $browser, string $title)
+    /**
+     * We need to check all the days for it to work because we wouldn't know on what day the date will turn out.
+     *
+     * @param Browser $browser
+     * @param string $title
+     * @param string $date
+     */
+    public function scheduleOnceFor(Browser $browser, string $title, string $date)
     {
-        $month = now();
-        $mm = in_array($month->month, [12, 11, 10]) ? "0{$month->month}" : $month->month;
         $browser->type('#title', $title)
-            ->type('#start_date', "{$mm}/15/2020")
-            ->type('#end_date', "{$mm}/15/2020")
+            ->type('#start_date', $date)
+            ->type('#end_date', $date)
             ->check("#mon")
             ->check("#tue")
             ->check("#wed")
